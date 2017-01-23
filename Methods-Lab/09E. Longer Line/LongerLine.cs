924a25170f37b7a079e65ea7172c561865fffa3b
VenciSarov <venci.sarov@gmail.com>
2017-01-23 08:47:20 +0200

    new task added and changes made it

--- Methods-Lab/09E. Longer Line/LongerLine.cs
+++ Methods-Lab/09E. Longer Line/LongerLine.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 class LongerLine
 {
@@ -15,48 +13,47 @@ class LongerLine
         double pointX4 = double.Parse(Console.ReadLine());
         double pointY4 = double.Parse(Console.ReadLine());
 
-        Console.WriteLine("(" + String.Join(", ",
-            GetLongerLine(pointX1, pointY1, pointX2, pointY2, pointX3, pointY3, pointX4, pointY4) + ")"));
-        GetNearestToZeroPoint(pointX1, pointY1, pointX2, pointY2, pointX3, pointY3, pointX4, pointY4);
+        PrintLongestLine(pointX1, pointY1, pointX2, pointY2, pointX3, pointY3, pointX4, pointY4);
     }
 
-    static double[] GetLongerLine(
-        double pointX1,
-        double pointY1,
-        double pointX2,
-        double pointY2,
-        double pointX3,
-        double pointY3,
-        double pointX4,
-        double pointY4)
+    static double GetFirstLine(double x1, double y1, double x2, double y2)
     {
-        double[] points = new double[2];
-
-        double firstLine = Math.Abs(Math.Sqrt(Math.Pow(pointX1, 2)) + Math.Pow(pointY2, 2) - 
-            (Math.Sqrt(Math.Pow(pointX2, 2)) + Math.Pow(pointY2,2)));
-        double secondLine = Math.Abs(Math.Sqrt(Math.Pow(pointX1, 2)) + Math.Pow(pointY2, 2) -
-           (Math.Sqrt(Math.Pow(pointX2, 2)) + Math.Pow(pointY2, 2)));
-
-        if (firstLine > secondLine)
-        {
-            points[]
-        }
+        return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
     }
-    static double GetNearestToZeroPoint(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
-    {
-        double[] points = new double[2];
 
-        if (Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2)) < Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2)))
+    static double GetSecondLine(double x3, double y3, double x4, double y4)
+    {
+        return Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+    }
+
+    static double GetNearestToCenterPoint(double x, double y)
+    {
+        return Math.Sqrt(Math.Pow((0 - x), 2) + Math.Pow((0 - y), 2));
+    }
+
+    static void PrintNearestToCenterPoint(double x1, double y1, double x2, double y2)
+    {
+        if (GetNearestToCenterPoint(x1, y1) > GetNearestToCenterPoint(x2, y2))
         {
-            points[0] = x1;
-            points[1] = y1;
-            return points;
+            Console.Write("(" + x2 + ", " + y2 + ")");
+            Console.WriteLine("(" + x1 + ", " + y1 + ")");
         }
         else
         {
-            points[0] = x2;
-            points[1] = y2;
-            return points;
+            Console.Write("(" + x1 + ", " + y1 + ")");
+            Console.WriteLine("(" + x2 + ", " + y2 + ")");
+        }
+    }
+
+    static void PrintLongestLine(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+    {
+        if (GetFirstLine(x1, y1, x2, y2) >= GetSecondLine(x3, y3, x4, y4))
+        {
+            PrintNearestToCenterPoint(x1, y1, x2, y2);
+        }
+        else
+        {
+            PrintNearestToCenterPoint(x3, y3, x4, y4);
         }
     }
 }
---
+++ Methods-Lab/10. Cube Properties/CubeProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class CubeProperties
+{
+    static void Main()
+    {
+        double cubeSide = double.Parse(Console.ReadLine());
+        string parameters = Console.ReadLine();
+
+        if (parameters == "face")
+            PrintFaceOfCube(cubeSide);
+        else if (parameters == "space")
+            PrintSpaceOfCube(cubeSide);
+        else if (parameters == "volume")
+            PrintVolumeOfCube(cubeSide);
+        else
+            PrintAreaOfCube(cubeSide);
+    }
+
+    static void PrintAreaOfCube(double cubeSide)
+    {
+        Console.WriteLine("{0:f2}", 6 * (Math.Pow(cubeSide, 2)));
+    }
+
+    static void PrintVolumeOfCube(double cubeSide)
+    {
+        Console.WriteLine("{0:f2}", Math.Pow(cubeSide, 3));
+    }
+
+    static void PrintSpaceOfCube(double cubeSide)
+    {
+        Console.WriteLine("{0:f2}", Math.Sqrt(3 * (Math.Pow(cubeSide, 2))));
+    }
+
+    static void PrintFaceOfCube(double cubeSide)
+    {
+        Console.WriteLine("{0:f2}", Math.Sqrt(2 * (Math.Pow(cubeSide, 2))));
+    }
+}
+
