c1334c9e52cda1bb432c2bab21587ed380cd4bed
VenciSarov <venci.sarov@gmail.com>
2017-01-24 08:42:07 +0200

    Changes in folders names

---
+++ Methods-Lab/12E. Master Numbers/MasterNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MasterNumbers
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+
+        //Console.WriteLine(isPalindromes(num)); 
+
+    }
+
+    //public static bool isPalindromes(int num)
+    //{
+    //    bool isPalindromes = false;
+
+    //}
+    public static  IsNumDivisableBySeven(int num)
+}
+
---
+++ Methods-Lab/14E. Factorial Trailing Zeroes/FactorialTrailingZeroes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+class FactorialTrailingZeroes
+{
+    static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetLastZerosFromFactorial(num));
+    }
+
+    static int GetLastZerosFromFactorial(int num)
+    {
+        BigInteger factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        int countZero = 0;
+
+        while (factorial > 0)
+        {
+            BigInteger digit = 0;
+            digit = factorial % 10;
+
+            if (digit != 0)
+            {
+                break;
+            }
+            else
+            {
+                countZero++;
+            }
+            factorial = factorial / 10;
+        }
+
+        return countZero;
+    }
+}
+
